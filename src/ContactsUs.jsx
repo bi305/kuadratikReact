57e175299d251ddc7b832271e04e96e14b838e49
bilal raza <razab0715@gmail.com>
2021-06-27 14:17:40 +0500

    animation  appled  on  contact us  page

--- src/ContactsUs.jsx
+++ src/ContactsUs.jsx
@@ -8,83 +8,95 @@ import BottomFooter from './components/BottomFooter';
 
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 import './css/contact.css';
+import ScrollAnimation from 'react-animate-on-scroll';
 const ContactsUs = () => {
     return (
         <>
-            <Container className="contact_bg " fluid="true" >
-                <Row className="overlay_contact   d-flex align-items-center ">
-                    <Col md='12'>
-                        <h1 className="text-center  who">
+            <ScrollAnimation animateIn="fadeIn">
+                <Container className="contact_bg " fluid="true" >
+                    <Row className="overlay_contact   d-flex align-items-center ">
 
-                            Contact Us
-                        </h1>
+                        <Col md='12'>
+                            <h1 className="text-center  who">
 
-                    </Col>
-                </Row>
-            </Container  >
+                                Contact Us
+                            </h1>
+
+                        </Col>
+                    </Row>
+                </Container  >
+            </ScrollAnimation>
             <Container className="mt-5">
                 <Row>
                     <Col md="6">
-                        <h1 className="my-5">  We're Ready, Let's Talk.</h1>
-                        <Form>
-                            <FormGroup>
+                        <ScrollAnimation animateIn="flipInY" className="">
+                            <h1 className="my-5">  We're Ready, Let's Talk.</h1>
+                            <Form>
+                                <FormGroup>
 
-                                <Input type="text" id="exampleEmail" placeholder="Name" />
-                            </FormGroup>
-                            <FormGroup>
+                                    <Input type="text" id="exampleEmail" placeholder="Name" />
+                                </FormGroup>
+                                <FormGroup>
 
-                                <Input type="email" id="examplePassword" placeholder="E-mail" />
-                            </FormGroup>
-                            <FormGroup>
+                                    <Input type="email" id="examplePassword" placeholder="E-mail" />
+                                </FormGroup>
+                                <FormGroup>
 
-                                <Input type="text" id="examplePassword" placeholder="Subject" />
-                            </FormGroup>
-                            <FormGroup>
+                                    <Input type="text" id="examplePassword" placeholder="Subject" />
+                                </FormGroup>
+                                <FormGroup>
 
-                                <Input type="textarea" id="exampleText" placeholder="Message" />
-                            </FormGroup>
+                                    <Input type="textarea" id="exampleText" placeholder="Message" />
+                                </FormGroup>
 
-                            <Button>Submit</Button>
-                        </Form>
+                                <Button>Submit</Button>
+                            </Form>
+                        </ScrollAnimation>
                     </Col>
-                    <Col md="6">
-                        <h1 className="my-5">
-                            Contact Details
-                        </h1>
-                        <p style={{ fontSize: '13px' }}>
-                            <b>
 
-                                Address
-                            </b>
-                        </p>
-                        <p>
+                    <Col md="6 " className="w-10">
+                        <ScrollAnimation animateIn="flipInY"    >
 
-                            109 Angier Crescent Ajax ON L1S 7R4
-                        </p>
-                        <p style={{ fontSize: '13px' }}>
-                            <b>
 
-                                Email Us
-                            </b>
-                        </p>
-                        <p>
-                            admin@kuadratik.com
-                        </p>
-                        <p style={{ fontSize: '13px' }}>
-                            <b>
+                            <h1 className="my-5">
+                                Contact Details
+                            </h1>
+                            <p style={{ fontSize: '13px' }}>
+                                <b>
 
-                                Call Us
-                            </b>
-                        </p>
-                        <p>
-                            +16474706922 ,  +16474706921
-                        </p>
-                        <p>
-                            <i class="mr-3 fa-2x fa fa-facebook-official" aria-hidden="true"></i>
-                            <i class="mx-3 fa-2x fa fa-twitter-square" aria-hidden="true"></i>
-                            <i class="mx-3 fa-2x fa fa-google-plus-square" aria-hidden="true"></i>
+                                    Address
+                                </b>
+                            </p>
+                            <p>
 
-                        </p>
+                                109 Angier Crescent Ajax ON L1S 7R4
+                            </p>
+                            <p style={{ fontSize: '13px' }}>
+                                <b>
+
+                                    Email Us
+                                </b>
+                            </p>
+                            <p>
+                                admin@kuadratik.com
+                            </p>
+                            <p style={{ fontSize: '13px' }}>
+                                <b>
+
+                                    Call Us
+                                </b>
+                            </p>
+                            <p>
+                                +16474706922 ,  +16474706921
+                            </p>
+                            <p>
+                                <i class="mr-3 fa-2x fa fa-facebook-official" aria-hidden="true"></i>
+                                <i class="mx-3 fa-2x fa fa-twitter-square" aria-hidden="true"></i>
+                                <i class="mx-3 fa-2x fa fa-google-plus-square" aria-hidden="true"></i>
+
+                            </p>
+
+                        </ScrollAnimation>
                     </Col>
                 </Row>
             </Container>
